060519412911f0085dedf65dd438e623c61776e1
Giel Berkers <giel.berkers@iodigital.com>
2023-05-10 14:08:31 +0200

    Open Drawer on menu button tap

--- app/src/main/java/com/gielberkers/barcodebuddy/views/MainView.kt
+++ app/src/main/java/com/gielberkers/barcodebuddy/views/MainView.kt
@@ -3,20 +3,27 @@ package com.gielberkers.barcodebuddy.views
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.DrawerValue
+import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.rememberDrawerState
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.gielberkers.barcodebuddy.ui.theme.BarCodeBuddyTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainView() {
     val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+    val coroutineScope = rememberCoroutineScope()
+
     Scaffold(
         scaffoldState = scaffoldState,
         topBar = {
@@ -25,8 +32,12 @@ fun MainView() {
                     Text("BarCodeBuddy")
                 },
                 navigationIcon = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        
+                    IconButton(onClick = {
+                        coroutineScope.launch {
+                            scaffoldState.drawerState.open()
+                        }
+                    }) {
+                        Icon(imageVector = Icons.Default.Menu, contentDescription = null)
                     }
                 }
             )
