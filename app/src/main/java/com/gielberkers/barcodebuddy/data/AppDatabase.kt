6998900e7f7ab48d8c2d8cefcb0853dbc98b2684
Giel Berkers <giel.berkers@iodigital.com>
2023-05-11 13:01:29 +0200

    Added database helper class

--- app/src/main/java/com/gielberkers/barcodebuddy/data/AppDatabase.kt
+++ app/src/main/java/com/gielberkers/barcodebuddy/data/AppDatabase.kt
@@ -1,9 +1,31 @@
 package com.gielberkers.barcodebuddy.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [BarCode::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun barCodeDao(): BarCodeDao
+}
+
+class DatabaseHelper {
+    @Volatile
+    private var appDatabase: AppDatabase? = null
+
+    fun getAppDatabase(ctx: Context): AppDatabase {
+        if (appDatabase == null) {
+            synchronized(this) {
+                if (appDatabase == null) {
+                    appDatabase = Room.databaseBuilder(
+                        ctx,
+                        AppDatabase::class.java, "app-database"
+                    )
+                    .build()
+                }
+            }
+        }
+        return appDatabase!!
+    }
 }
